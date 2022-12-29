30f30ba1c82f255ca077e79cbc30b41b5b3684f5
AKSHAT <akshat@192.168.1.3>
2022-12-29 23:50:12 +0530

    create-react-app and initialize general styles and components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Education from './components/education/Education'
+import Experience from './components/xp/Experience'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Education />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App
